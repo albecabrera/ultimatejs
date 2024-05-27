61d47ee6161c3491f89543d71fe0f5a2e8761d58
albecabrera <albe.cabrera@yahoo.de>
2024-05-27 22:28:02 +0200

    commit "objetos y arreglos"

---
+++ 01-tipos/02-primitivos.js
@@ -0,0 +1,13 @@
+let numero = 1;
+let texto = "Hola mundo";
+let booleano = true;
+let booleanofalso = false;
+let arreglo = [1, 2, 3];
+let objeto = { nombre: "Juan", apellido: "Perez" };
+
+console.log(numero);
+console.log(texto);
+console.log(booleano);
+console.log(arreglo);
+console.log(objeto);
+console.log(booleanofalso);

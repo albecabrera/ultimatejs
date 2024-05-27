ee63c6674e73bfba193b42461f673d32c798137f
albecabrera <albe.cabrera@yahoo.de>
2024-05-27 13:01:29 +0200

    commit "primer cambio"

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,24 @@
+// declarar una variable
+let nombre = "Hola Mundo";
+let NombreCompleto;
+let nombreCompleto;
+let nombre_completo;
+
+console.log(NombreCompleto);
+
+// UpperCamelCase => todos las letras comienzan con mayúscula
+// lowerCamelCase => la primer letra es minúscula
+// PascalCase => todas las letras comienzan con mayúscula
+// snake_case => las palabras separadas por guiones bajos
+
+let apellido;
+let animal;
+apellido = "Perez";
+animal = "Gato";
+console.log(animal);
+console.log(apellido);
+
+
+// Datos primitivos: string, number, boolean, null, undefined
+// Datos de referencia: array, object, functions, clases
+

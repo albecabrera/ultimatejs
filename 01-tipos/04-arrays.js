947b14aca3770b6771733a38808409ae250c1b63
albecabrera <albe.cabrera@yahoo.de>
2024-05-27 23:02:43 +0200

    commit "funciones"

---
+++ 01-tipos/04-arrays.js
@@ -0,0 +1,11 @@
+// Arrays o arreglos
+
+let animales = ["chanchito", "Caballo"];
+
+console.log(animales);
+console.log(animales[0]);
+animales[2] = "Dragón";
+console.log(animales);
+
+animales[3] = "Perro";
+console.log(animales);

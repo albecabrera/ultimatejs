ea60db5862efc7095c52165688aedd38f12fe000
albecabrera <albe.cabrera@yahoo.de>
2024-05-28 22:11:45 +0200

    commit -m "agregando los while"

---
+++ 03-control-flujo/02-else.js
@@ -0,0 +1,9 @@
+let edad = 10;
+
+if (edad > 17) {
+  console.log("Eres mayor de edad");
+} else if (edad > 13) {
+  console.log("Eres un adolescente");
+} else {
+  console.log("Eres menor de edad");
+}

846452be5fa9976f83315761ed9785fc3c2fa346
albecabrera <albe.cabrera@yahoo.de>
2024-05-29 08:30:18 +0200

    commit "cambios"

---
+++ 03-control-flujo/07-for-of.js
@@ -0,0 +1,14 @@
+// while
+// do while
+// for
+
+let animales = ["chanchito", "caballo", "perro", "gato"];
+for (let animal of animales) {
+  console.log(animal);
+}
+
+let i = 0;
+while (i < animales.length) {
+  console.log(animales[i]);
+  i++;
+}
---
+++ 03-control-flujo/10-switch.js
@@ -0,0 +1,18 @@
+let action = "actualizar";
+
+switch (action) {
+  case "listar":
+    console.log("Listar");
+    break;
+  case "agregar":
+    console.log("Agregar");
+    break;
+  case "modificar":
+    console.log("modificar");
+    break;
+  case "borrar":
+    console.log("borrar");
+    break;
+  default:
+    console.log("No existe la accion");
+}

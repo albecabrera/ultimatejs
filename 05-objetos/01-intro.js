9c2e6c554deaf4e1831f62226880b502b883d02f
albecabrera <albe.cabrera@yahoo.de>
2024-06-04 12:17:23 +0200

    commit -m "POO => Programación orientada a objetos.

---
+++ 05-objetos/01-intro.js
@@ -0,0 +1,19 @@
+
+
+let user = 'nico@holamundo.io';
+email: 'nico@holamundo.io', 
+name = 'Nicolas',
+direccion: {
+  calle: 'Queen st',
+  numero: 15,
+}
+
+let direccion = {
+  calle: 'Queen st',
+  numero: 15, 
+};
+
+activo: true, 
+recuperarClave: function () {
+  console.log('recuperar clave');
+}
